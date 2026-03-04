a99f05a62b984ff81e27a8fdc24411102ba1fc22
mzcheong97 <mark@intermatch.co>
2026-03-04 08:41:59 -0800

    Update intial

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,6 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import { createBrowserRouter } from 'react-router-dom';
-const router = createBrowserRouter( /* application routes are defined here */ );
-
 
 test('renders learn react link', () => {
   const { getByText } = render(
