5aa781b923b2322b71584ce09010e7e9e4110904
mzcheong97 <mark@intermatch.co>
2026-03-09 07:05:24 -0700

    replace /a with /link

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -22,9 +22,9 @@ export default function Articles () {
         { filteredArticles.map(article => (
           <li key={article.slug}>
             {/* Replace these a tags! */}
-            <a href={`${article.slug}`}>
+            <Link to href={`${article.slug}`}>
               {article.title}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
