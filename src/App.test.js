01498f8e113da11b4671b44b8d2362c02fcba7ae
mzcheong97 <mark@intermatch.co>
2026-03-10 10:01:00 -0700

    fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-const router = createBrowserRouter( /* application routes are defined here */ );
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+
+const router = createBrowserRouter( /* application routes are defined here */ );
 
 test('renders learn react link', () => {
   const { getByText } = render(
