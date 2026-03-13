a31966132774c33fa0f9457cda9d9a8a87768ede
mzcheong97 <mark@intermatch.co>
2026-03-13 10:28:04 -0700

    article searchparams

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -2,7 +2,7 @@ import { React } from "react";
 import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useSearchParams } from "react-router-dom";
 
 // Import Link and useSearchParams from React Router
 
@@ -11,10 +11,12 @@ export default function Articles () {
   const { title } = useParams();
   const article = articles[title];
   // Grab URLSearchParams object from useSearchParams hook
+  const [searchParams] = useSearchParams();
   
   // Get the queryParams from object returned from useSearchParams and set to `title`
+  const queryTitle = searchParams.get("title");
 
-  const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
+  const filteredArticles = queryTitle ? filterArticles(queryTitle, articles) : Object.values(articles)
 
   return (
     <main>
